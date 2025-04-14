e6829d39a1fe1fe760702d3c7aee5607fa047198
Jatin Maurya <mauryajatin45@gmail.com>
2025-04-14 12:22:44 +0530

    is authenticated has been setupped

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -4,7 +4,14 @@ const User = require('../models/User');
 const { model } = require('mongoose');
 const router = express.Router();
 
-router.get('/', (req, res)=>{
+function isAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+      return next();  // Continue if authenticated
+  }
+  res.redirect('/login');  // Redirect to login if not authenticated
+}
+
+router.get('/', isAuthenticated, (req, res)=>{
     res.render("home.ejs", {user:req.user})
   })
 
